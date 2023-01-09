7cab6b73b570a50195614c736ac31d1e87379f85
Alexandr Siryj <theobroma222@gmail.com>
2023-01-09 19:15:53 +0200

    first commit

---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,13 @@
+import { Container, Box } from '@mui/material';
+
+import { DropdownShareButton } from '@/components/DropdownShareButton/DropdownShareButton';
+
+const HomePage = () => (
+  <Container maxWidth="lg">
+    <Box>HomePage</Box>
+    <DropdownShareButton />
+  </Container>
+);
+
+// eslint-disable-next-line import/no-default-export
+export default HomePage;
