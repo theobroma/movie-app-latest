65e0f91e15ad248d6dfc876c463d448cb1689c36
Alexandr Siryj <theobroma222@gmail.com>
2023-01-10 17:19:54 +0200

    LanguageMenu

--- src/components/AppBar/Drawer.tsx
+++ src/components/AppBar/Drawer.tsx
@@ -16,6 +16,7 @@ import { BackToTop } from '@/atoms/BackToTop/BackToTop';
 import { HideOnScroll } from '@/atoms/HideOnScroll/HideOnScroll';
 
 import { AppBar, DrawerHeader, drawerWidth, Main } from './Drawer.styled';
+import { LanguageMenu } from './LanguageMenu/LanguageMenu';
 import { NestedList } from './NestedList/NestedList';
 import { ThemeMenu } from './ThemeMenu/ThemeMenu';
 
@@ -57,7 +58,8 @@ export const PersistentDrawerLeft = ({ children }: PropsWithChildren) => {
               </Typography>
             </RouterLink>
             <Box sx={{ flexGrow: 1 }} />
-            {/* Theme Menu */}
+            {/* Menus */}
+            <LanguageMenu />
             <ThemeMenu />
           </Toolbar>
         </AppBar>
--- src/store/ui/selectors.ts
+++ src/store/ui/selectors.ts
@@ -1,5 +1,14 @@
 import { RootState } from '@/store/configureStore';
+import { alpha2iso } from '@/utils/alpha2iso';
 
 export const themeSelector = (state: RootState) => {
   return state.ui.theme;
+};
+
+export const languageSelector = (state: RootState) => {
+  return state.ui.language;
+};
+
+export const languageISOSelector = (state: RootState) => {
+  return alpha2iso(state.ui.language);
 };
