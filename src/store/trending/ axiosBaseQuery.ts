3f44f0559fe822e986669d098467c01b077cec22
Alexandr Siryj <theobroma222@gmail.com>
2023-01-10 19:58:26 +0200

    moviedbBaseQuery

---
+++ src/store/trending/ axiosBaseQuery.ts
@@ -0,0 +1,59 @@
+import { BaseQueryFn } from '@reduxjs/toolkit/query/react';
+import { AxiosError, AxiosRequestConfig } from 'axios';
+
+import { axiosInstance } from '@/api/connection';
+import type { RootState } from '@/store/configureStore';
+import { alpha2iso } from '@/utils/alpha2iso';
+import { waitForMe } from '@/utils/waitforme';
+
+// const axiosBaseQuery =
+//   (): BaseQueryFn<any> =>
+//   async (requestOpts, { getState }) => {
+//     try {
+//       const languageAlpha2 = (getState() as RootState).ui.language;
+//       const languageISO = alpha2iso(languageAlpha2);
+
+//       console.log('languageISO', languageISO);
+
+//       const result = await axiosInstance({
+//         ...requestOpts,
+//       });
+
+//       return { data: result.data };
+//     } catch (axiosError) {
+//       const err = axiosError as AxiosError;
+//       return {
+//         error: { status: err.response?.status, data: err.response?.data },
+//       };
+//     }
+//   };
+
+// export const moviedbBaseQuery = axiosBaseQuery();
+
+export const moviedbBaseQuery =
+  ({ baseUrl }: { baseUrl: string } = { baseUrl: '' }): BaseQueryFn<any> =>
+  async ({ url, method, data }) => {
+    try {
+      await waitForMe(1000);
+      const res = await axiosInstance({
+        url: baseUrl + url,
+        method,
+        data,
+      });
+      // ZOD validation
+      //   try {
+      //     PicturesDataResponseSchema.parse(res.data);
+      //   } catch (error) {
+      //     // TODO:
+      //     // Log & alert error <-- very important!
+      //     console.log(error);
+      //   }
+
+      return { data: res.data };
+    } catch (axiosError) {
+      const err = axiosError as AxiosError;
+      return {
+        error: { status: err.response?.status, data: err.response?.data },
+      };
+    }
+  };
--- src/store/trending/api.ts
+++ src/store/trending/api.ts
@@ -2,21 +2,27 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { API_URL, API_KEY } from '@/api/connection';
 
+import { moviedbBaseQuery } from './ axiosBaseQuery';
+
 export const trendingAPI = createApi({
-  baseQuery: fetchBaseQuery({
+  baseQuery: moviedbBaseQuery({
     baseUrl: `${API_URL}`,
   }),
   endpoints: (builder) => ({
     trendingTV: builder.query<any, { page: number | void; isoCode: string }>({
-      query: ({ page, isoCode }) =>
-        `/trending/tv/week?page=${page}&api_key=${API_KEY}&language=${isoCode}`,
+      query: ({ page, isoCode }) => ({
+        url: `/trending/tv/week?page=${page}&api_key=${API_KEY}&language=${isoCode}`,
+        method: 'get',
+      }),
     }),
     trendingMovies: builder.query<
       any,
       { page: number | void; isoCode: string }
     >({
-      query: ({ page, isoCode }) =>
-        `/trending/movie/week?page=${page}&api_key=${API_KEY}&language=${isoCode}`,
+      query: ({ page, isoCode }) => ({
+        url: `/trending/movie/week?page=${page}&api_key=${API_KEY}&language=${isoCode}`,
+        method: 'get',
+      }),
     }),
   }),
 });
