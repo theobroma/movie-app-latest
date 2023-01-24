976fde5c6ea8f637f79f52b18573b07ce4071d5e
Alexandr Siryj <theobroma222@gmail.com>
2023-01-25 01:17:03 +0200

    detailsAPI rtkq

--- src/pages/media-details/media-details-info/media-details-info.tsx
+++ src/pages/media-details/media-details-info/media-details-info.tsx
@@ -1,12 +1,13 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Box, Container } from '@mui/material';
 
 import { MediaDetailsRouteParams } from '@/pages/media-details/media-details.interface';
 import { useAppDispatch, useAppSelector } from '@/store/configureStore';
-import { mediaDetailsSelector } from '@/store/details/selectors';
-import { getMediaDetailsTC } from '@/store/details/slice';
+import { useDetailsMediaQuery } from '@/store/details/api';
+// import { mediaDetailsSelector } from '@/store/details/selectors';
+// import { getMediaDetailsTC } from '@/store/details/slice';
 import { languageISOSelector } from '@/store/ui/selectors';
 
 import { MediaDetailsInfoDataSkeleton } from './media-details-info-data-skeleton/media-details-info-data-skeleton';
@@ -16,27 +17,36 @@ import { useStyles } from './media-details-info.styles';
 const backdropBase = 'https://image.tmdb.org/t/p/original';
 
 export const MediaDetailsInfo = () => {
+  const { classes } = useStyles();
+  const dispatch = useAppDispatch();
+  // just for useEffect refetch if changed
+  const langISOCode = useAppSelector(languageISOSelector);
   const { mediaId, mediaType } = useParams<
     keyof MediaDetailsRouteParams
   >() as MediaDetailsRouteParams;
 
-  const { classes } = useStyles();
-  const dispatch = useAppDispatch();
+  // const {
+  //   data: mediaDetailsData,
+  //   error,
+  //   isError,
+  //   isLoading,
+  //   isSuccess,
+  // } = useAppSelector(mediaDetailsSelector);
+
+  // useEffect(() => {
+  //   if (mediaId && mediaType) {
+  //     dispatch(getMediaDetailsTC({ mediaId, mediaType }));
+  //   }
+  // }, [dispatch, mediaId, mediaType, langISOCode]);
+
   const {
     data: mediaDetailsData,
-    error,
-    isError,
+    // error,
+    // isError,
+    // isSuccess,
     isLoading,
-    isSuccess,
-  } = useAppSelector(mediaDetailsSelector);
-  // just for useEffect refetch if changed
-  const langISOCode = useAppSelector(languageISOSelector);
-
-  useEffect(() => {
-    if (mediaId && mediaType) {
-      dispatch(getMediaDetailsTC({ mediaId, mediaType }));
-    }
-  }, [dispatch, mediaId, mediaType, langISOCode]);
+    // isFetching,
+  } = useDetailsMediaQuery({ mediaId, mediaType, isoCode: langISOCode });
 
   const mediaTitle =
     mediaDetailsData?.title ||
---
+++ src/store/details/api.ts
@@ -0,0 +1,20 @@
+import { MediaTypeEnum } from '@/enums/media-type.enum';
+import { emptySplitApi } from '@/store/emptySplitApi';
+
+const detailsAPI = emptySplitApi.injectEndpoints({
+  endpoints: (builder) => ({
+    detailsMedia: builder.query<
+      any,
+      { mediaType: MediaTypeEnum; mediaId: string; isoCode: string }
+    >({
+      query: ({ mediaType, mediaId, isoCode }) => ({
+        url: `/${mediaType}/${mediaId}?language=${isoCode}`,
+        method: 'get',
+      }),
+      // providesTags: ['DetailsMedia'],
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const { useDetailsMediaQuery } = detailsAPI;
