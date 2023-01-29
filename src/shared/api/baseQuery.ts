abdd5e3fb22dd37c3c190cca74dd9439156ab7ff
Alexandr Siryj <theobroma222@gmail.com>
2023-01-29 03:09:01 +0200

    zod-validation-error

--- src/shared/api/baseQuery.ts
+++ src/shared/api/baseQuery.ts
@@ -1,7 +1,9 @@
 import { BaseQueryFn } from '@reduxjs/toolkit/query/react';
 import { AxiosError } from 'axios';
+import { fromZodError } from 'zod-validation-error';
 
 import { axiosInstance } from '@/shared/api/connection';
+import { DetailsMovieSchema, DetailsTVSchema } from '@/types';
 import { waitForMe } from '@/utils/waitforme';
 
 export const baseQuery =
@@ -15,20 +17,34 @@ export const baseQuery =
         data,
       });
 
+      const splittedUrl = url.split('?')[0];
+      const asPathNestedRoutes = splittedUrl
+        .split('/')
+        .filter((v: any) => v.length > 0);
+
       // console.log('url :>> ', url);
-      // const splittedUrl = url.split('?')[0];
       // console.log('splittedUrl :>> ', splittedUrl);
-      // const asPathNestedRoutes = splittedUrl
-      //   .split('/')
-      //   .filter((v: any) => v.length > 0);
       // console.log('asPathNestedRoutes :>> ', asPathNestedRoutes);
 
       // ZOD validation
       try {
+        console.log('res.data :>> ', res.data);
         // TVResponseSchema.parse(res.data);
         // MoviesResponseSchema.parse(res.data);
-      } catch (error) {
-        console.log(error);
+        // DetailsTVSchema.parse(res1.data);
+        // if (param.mediaType === MEDIA_TYPE.TV) {
+        //   DetailsTVSchema.parse(res1.data);
+        // }
+        // if (asPathNestedRoutes[0] === 'movie') {
+        //   DetailsMovieSchema.parse(res.data);
+        // }
+      } catch (error: any) {
+        // console.log(error);
+        const validationError = fromZodError(error);
+        // the error now is readable by the user
+        // you may print it to console
+        // or return it via an API
+        console.log(validationError);
       }
 
       return { data: res.data };
---
+++ src/types/details/index.ts
@@ -0,0 +1,3 @@
+export * from './z.details-movie';
+export * from './z.details-shared';
+export * from './z.details-tv';
