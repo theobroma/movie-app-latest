804b58eda14bc41800a209602dead5a96ded694e
Alexandr Siryj <theobroma222@gmail.com>
2023-02-01 21:02:10 +0200

    FavouritesTvshowsPage

--- src/pages/favourites/favourites-tvshows/favourites-tvshows.page.tsx
+++ src/pages/favourites/favourites-tvshows/favourites-tvshows.page.tsx
@@ -1,8 +1,27 @@
-const FavouritesTvshowsPage = () => (
-  <div>
-    <span>FavouritesTvshowsPage</span>
-  </div>
-);
+import { nanoid } from '@reduxjs/toolkit';
 
+import { Grid } from '@mui/material';
+
+import { MediaTypeEnum } from '@/enums/media-type.enum';
+import { favouritesTVSelector } from '@/features/favourites/store/selectors';
+import { MediaCardFetch } from '@/features/media-card-fetch/media-card-fetch';
+import { useAppSelector } from '@/store/configureStore';
+
+const FavouritesTvshowsPage = () => {
+  const favouritesTV = useAppSelector(favouritesTVSelector);
+
+  return (
+    <>
+      <span>FavouritesMoviesPage</span>
+      <Grid container spacing={2} sx={{ mt: '16px' }}>
+        {favouritesTV.map((tvId: any, idx: number) => (
+          <Grid item xs={6} sm={4} md={4} lg={2} key={nanoid()}>
+            <MediaCardFetch mediaId={tvId} mediaType={MediaTypeEnum.TV} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
 // eslint-disable-next-line import/no-default-export
 export default FavouritesTvshowsPage;
