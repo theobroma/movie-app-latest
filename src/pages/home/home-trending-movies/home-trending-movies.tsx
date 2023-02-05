06ca6b88b593b79ae230e8002a49d3dc8d75fe25
Alexandr Siryj <theobroma222@gmail.com>
2023-02-05 10:01:22 +0200

    migrate to MediaList

--- src/pages/home/home-trending-movies/home-trending-movies.tsx
+++ src/pages/home/home-trending-movies/home-trending-movies.tsx
@@ -1,14 +1,10 @@
-import { nanoid } from '@reduxjs/toolkit';
+import { Grid } from '@mui/material';
 
-import { Alert, Grid, Stack } from '@mui/material';
-
-import { MediaCardMovie } from '@/entities/media/ui/MediaCard/MediaCardMovie/MediaCardMovie';
-import { MediaCardSkeleton } from '@/entities/media/ui/MediaCard/MediaCardSkeleton/MediaCardSkeleton';
-import { AppError } from '@/shared/uikit/AppError/AppError';
-import { isEmptyArray } from '@/shared/utils/is-data';
+import { MediaTypeEnum } from '@/enums/media-type.enum';
 import { useAppSelector } from '@/store/configureStore';
 import { useTrendingMoviesQuery } from '@/store/trending/api';
 import { timeWindowsSelector } from '@/store/trending/selectors';
+import { MediaList } from '@/widgets/media-list/media-list';
 
 import { HomeTrendingMoviesLinks } from './home-trending-movies-links/home-trending-movies-links';
 
@@ -21,46 +17,19 @@ export const HomeTrendingMovies = () => {
     isSuccess,
     isFetching,
   } = useTrendingMoviesQuery({ page: 1, timeWindows });
-  // Slice just first 6
-  const trendingMovies = moviesData?.results.slice(0, 6);
-  const skeletons = Array(6).fill('none');
 
   return (
-    <Grid container spacing={2} sx={{ mt: '16px' }}>
+    <Grid container spacing={2} sx={{ mt: 2 }}>
       <HomeTrendingMoviesLinks />
-      {!isError &&
-        !isFetching &&
-        !isEmptyArray(trendingMovies) &&
-        trendingMovies?.map((movie: any) => (
-          <>
-            <Grid item xs={6} sm={4} md={4} lg={2} key={nanoid()}>
-              <MediaCardMovie movie={movie} />
-            </Grid>
-            {/* <Grid item xs={6} sm={4} md={4} lg={2} key={nanoid()}>
-              <MediaCardSkeleton />
-            </Grid> */}
-          </>
-        ))}
-      {isFetching &&
-        skeletons.map(() => (
-          <Grid item xs={6} sm={4} md={4} lg={2} key={nanoid()}>
-            <MediaCardSkeleton />
-          </Grid>
-        ))}
-      {/* no results */}
-      {isSuccess && isEmptyArray(trendingMovies) && (
-        <Grid item xs={12}>
-          <Stack sx={{ width: '100%' }} spacing={2}>
-            <Alert severity="warning">There is no data</Alert>
-          </Stack>
-        </Grid>
-      )}
-      {/* error */}
-      {isError && (
-        <Grid item xs={12}>
-          <AppError error={error} />
-        </Grid>
-      )}
+      <MediaList
+        mediaData={moviesData}
+        mediaType={MediaTypeEnum.Movie}
+        itemsToShow={6}
+        isError={isError}
+        isFetching={isFetching}
+        isSuccess={isSuccess}
+        error={error}
+      />
     </Grid>
   );
 };
--- src/pages/home/home-trending-tvshows/home-trending-tvshows.tsx
+++ src/pages/home/home-trending-tvshows/home-trending-tvshows.tsx
@@ -1,14 +1,10 @@
-import { nanoid } from '@reduxjs/toolkit';
+import { Grid } from '@mui/material';
 
-import { Alert, Grid, Stack } from '@mui/material';
-
-import { MediaCardSkeleton } from '@/entities/media/ui/MediaCard/MediaCardSkeleton/MediaCardSkeleton';
-import { MediaCardTV } from '@/entities/media/ui/MediaCard/MediaCardTV/MediaCardTV';
-import { AppError } from '@/shared/uikit/AppError/AppError';
-import { isEmptyArray } from '@/shared/utils/is-data';
+import { MediaTypeEnum } from '@/enums/media-type.enum';
 import { useAppSelector } from '@/store/configureStore';
 import { useTrendingTVQuery } from '@/store/trending/api';
 import { timeWindowsSelector } from '@/store/trending/selectors';
+import { MediaList } from '@/widgets/media-list/media-list';
 
 import { HomeTrendingTvshowsLinks } from './home-trending-tvshows-links/home-trending-tvshows-links';
 
@@ -24,41 +20,19 @@ export const HomeTrendingTVShows = () => {
     page: 1,
     timeWindows,
   });
-  // Slice just first 6
-  const trendingTVShows = tvData?.results.slice(0, 6);
-  const skeletons = Array(6).fill('none');
 
   return (
-    <Grid container spacing={2} sx={{ mt: '16px' }}>
+    <Grid container spacing={2} sx={{ mt: 2 }}>
       <HomeTrendingTvshowsLinks />
-      {!isError &&
-        !isFetching &&
-        !isEmptyArray(trendingTVShows) &&
-        trendingTVShows?.map((tvshow) => (
-          <Grid item xs={6} sm={4} md={4} lg={2} key={nanoid()}>
-            <MediaCardTV tvshow={tvshow} />
-          </Grid>
-        ))}
-      {isFetching &&
-        skeletons.map(() => (
-          <Grid item xs={6} sm={4} md={4} lg={2} key={nanoid()}>
-            <MediaCardSkeleton />
-          </Grid>
-        ))}
-      {/* no results */}
-      {isSuccess && isEmptyArray(trendingTVShows) && (
-        <Grid item xs={12}>
-          <Stack sx={{ width: '100%' }} spacing={2}>
-            <Alert severity="warning">There is no data</Alert>
-          </Stack>
-        </Grid>
-      )}
-      {/* error */}
-      {isError && (
-        <Grid item xs={12}>
-          <AppError error={error} />
-        </Grid>
-      )}
+      <MediaList
+        mediaData={tvData}
+        mediaType={MediaTypeEnum.TV}
+        itemsToShow={6}
+        isError={isError}
+        isFetching={isFetching}
+        isSuccess={isSuccess}
+        error={error}
+      />
     </Grid>
   );
 };
