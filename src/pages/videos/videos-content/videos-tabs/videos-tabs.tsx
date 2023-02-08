c9693774ba86538c5f4b4801698c3e43fa904153
Alexandr Siryj <theobroma222@gmail.com>
2023-02-08 16:31:07 +0200

    dynamic VideosTabs

--- src/pages/videos/videos-content/videos-tabs/videos-tabs.tsx
+++ src/pages/videos/videos-content/videos-tabs/videos-tabs.tsx
@@ -1,51 +1,55 @@
+import { useEffect, useState } from 'react';
+
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import { Badge } from '@mui/material';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-
-import { useAppDispatch, useAppSelector } from '@/store/configureStore';
-import { timeWindowsSelector } from '@/store/trending/selectors';
-import { setTimeWindowsAC } from '@/store/trending/slice';
 
 interface Props {
   groupedVideos: any;
 }
 
 export const VideosTabs = ({ groupedVideos }: Props) => {
-  const dispatch = useAppDispatch();
-  const timeWindows = useAppSelector(timeWindowsSelector);
+  const tabKeys = Object.keys(groupedVideos);
+  const [value, setValue] = useState('');
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    dispatch(setTimeWindowsAC(newValue));
+  useEffect(() => {
+    setValue(tabKeys[0]);
+  }, [groupedVideos]);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
   };
 
-  console.log('groupedVideos', groupedVideos);
-  const tabKeys = Object.keys(groupedVideos);
-
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Tabs
-        value={timeWindows}
-        onChange={handleChange}
-        aria-label="time windows tabs"
-      >
-        {/* <Tab label="Day" value={TimeWindowsEnum.Day} />
-        <Tab label="Week" value={TimeWindowsEnum.Week} /> */}
-        {tabKeys.map((tab) => (
-          <Tab
-            label={
-              <Box sx={{ display: 'flex' }}>
-                <Box>{tab}</Box>
-                <Badge
-                  badgeContent={groupedVideos[tab].length}
-                  color="primary"
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      {value && (
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              {tabKeys.map((tab) => (
+                <Tab
+                  label={
+                    <Box sx={{ display: 'flex' }}>
+                      <Box>{tab}</Box>
+                      <Badge
+                        badgeContent={groupedVideos[tab].length}
+                        color="primary"
+                      />
+                    </Box>
+                  }
+                  value={tab}
                 />
-              </Box>
-            }
-            value={tab}
-          />
-        ))}
-      </Tabs>
+              ))}
+            </TabList>
+          </Box>
+          {tabKeys.map((tab, idx: number) => (
+            <TabPanel value={tab}>{idx}</TabPanel>
+          ))}
+        </TabContext>
+      )}
     </Box>
   );
 };
